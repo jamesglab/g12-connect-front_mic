0ca39e9539208820fd840c491b4895608d0302b2
NicolasSalazar <desarrollo@gracialab.co>
2021-08-30 17:46:25 -0500

    change variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,38 @@
+// export const environment = {
+//   production: true,
+//   appVersion: 'v710demo1', //METRONIC
+//   USERDATA_KEY: 'authf649fc9a5f55', // METRONIC
+//   isMockEnabled: false, // METRONIC
+//   apiUrl: 'https://plataforma.mci12.com:444/', //.NETAPI
+//   apiUrlG12Connect: 'https://api.g12connect.com/api/v2/'
+// };
+
+// // This file can be replaced during build by using the `fileReplacements` array.
+// // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// // The list of file replacements can be found in `angular.json`.
+
 export const environment = {
   production: true,
   appVersion: 'v710demo1', //METRONIC
   USERDATA_KEY: 'authf649fc9a5f55', // METRONIC
   isMockEnabled: false, // METRONIC
-  apiUrl: 'https://plataforma.mci12.com:444/', //.NETAPI
-  apiUrlG12Connect: 'https://api.g12connect.com/api/v2/'
+  apiUrl: 'http://51.79.78.171:7003/', //.NETAPI
+  // apiUrlG12Connect: 'https://dev-api.g12connect.com/api/v2/',
+  apiUrlG12Connect: {
+    donations: 'https://dev-api.g12connect.com/api/v2/donations',
+    managment: 'https://dev-api.g12connect.com/api/v2/managment',
+    users: 'https://dev-api.g12connect.com/api/v2/users',
+    payments: 'https://dev-api.g12connect.com/api/v2/payments',
+    reports : 'https://dev-api.g12connect.com/api/v2/reports'
+  },
 };
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+ 
