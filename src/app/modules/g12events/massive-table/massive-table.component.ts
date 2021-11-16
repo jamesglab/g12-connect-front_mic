612dad9b21d4abbafdcc2e4114a1c45e0c9fff5c
Miguel torres <lt@wap.us>
2021-11-16 15:22:11 -0500

    compile

--- src/app/modules/g12events/massive-table/massive-table.component.ts
+++ src/app/modules/g12events/massive-table/massive-table.component.ts
@@ -75,7 +75,7 @@ export class MassiveTableComponent implements OnInit {
   addUser(transaction) {
     //CREAMOE EL MODAL Y ABRIMOS EL COMPONENTE DE EditEventComponent
     console.log('status',parseInt(transaction.transaction.status))
-    if (parseInt(transaction.transaction.status) != 3) {
+    if (parseInt(transaction.transaction.status) != 1) {
       Swal.fire('Massivo inactivo', 'No se ha procesado la solicitud del pago intenta mas tarde', 'info');
       return
     }
