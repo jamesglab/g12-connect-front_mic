aceb1bbf89d97d38edac39d82798ddcd8c0f163c
Miguel torres <lt@wap.us>
2021-12-02 15:07:05 -0500

    massive add user

--- src/app/modules/g12events/massive-table/components/add-user/add-user.component.ts
+++ src/app/modules/g12events/massive-table/components/add-user/add-user.component.ts
@@ -11,7 +11,7 @@ import { G12eventsService } from '../../../_services/g12events.service';
 @Component({
   selector: 'app-add-user',
   templateUrl: './add-user.component.html',
-  styleUrls: ['./add-user.component.scss']
+  styleUrls: ['./add-user.component.scss'],
 })
 export class AddUserMassiveComponent implements OnInit {
   public add_user: FormGroup;
@@ -20,21 +20,26 @@ export class AddUserMassiveComponent implements OnInit {
   public leaders: [] = [];
   public pastors: [] = [];
   public churchs = [];
-  public countries: any[] = COUNTRIES;//LISTADO DE PAISES;
+  public countries: any[] = COUNTRIES; //LISTADO DE PAISES;
 
   public isLoading: boolean = false;
   public find_user: boolean = false;
-  constructor(private fb: FormBuilder, public modal: NgbActiveModal, private g12EventService: G12eventsService,
-    public cdr: ChangeDetectorRef, private userService: UserService, private storageService: StorageService) { }
+  constructor(
+    private fb: FormBuilder,
+    public modal: NgbActiveModal,
+    private g12EventService: G12eventsService,
+    public cdr: ChangeDetectorRef,
+    private userService: UserService,
+    private storageService: StorageService
+  ) {}
 
   ngOnInit(): void {
     this.buildForm();
     this.getChurchs(this.currentUser.church_id);
   }
 
-
   //*************************/
-  // OPCIONES DEL FORMULARIO 
+  // OPCIONES DEL FORMULARIO
   //************************/
 
   // CREAMOS EL FORMULARIO
@@ -42,15 +47,24 @@ export class AddUserMassiveComponent implements OnInit {
     this.add_user = this.fb.group({
       transaction_id: [this.transaction.transaction.id, Validators.required],
       id: [null],
-      country: [this.currentUser.church_id ? 'Colombia' : null, Validators.required],
+      country: [
+        this.currentUser.church_id ? 'Colombia' : null,
+        Validators.required,
+      ],
       document: [null, Validators.required],
       document_type: [null],
       name: [null, Validators.required],
       last_name: [null, Validators.required],
       gender: [null, Validators.required],
       phone: [null, Validators.required],
-      email: [null, Validators.compose([Validators.required, Validators.email])],
-      confirm_email: [null, Validators.compose([Validators.required, Validators.email])],
+      email: [
+        null,
+        Validators.compose([Validators.required, Validators.email]),
+      ],
+      confirm_email: [
+        null,
+        Validators.compose([Validators.required, Validators.email]),
+      ],
       leader: [{ value: null, disabled: true }, Validators.required],
       network: [null],
       type_church: ['MCI'],
@@ -70,85 +84,119 @@ export class AddUserMassiveComponent implements OnInit {
     this.pastors = [];
     this.form_controls.leader.disable();
     this.form_controls.pastor.disable();
-    this.userService.getLeadersOrPastors({ userCode: user_code, church: user ? user.church_id : this.currentUser.church_id }).subscribe(res => {
-      this.pastors = res;
-      this.form_controls.pastor.enable();//INHABILITAMOS EL SELECTOR DEL PASTOR
-      if (user) {
-        console.log('selector', res.find(pt => pt.user_code == user.pastor_code));
-        this.form_controls.pastor.setValue(res.find(pt => pt.user_code == user.pastor_code));
-        this.form_controls.pastor.disable();
-      }
-    }, err => {
-      throw err
-    });
+    this.userService
+      .getLeadersOrPastors({
+        userCode: user_code,
+        church: user ? user.church_id : this.currentUser.church_id,
+      })
+      .subscribe(
+        (res) => {
+          this.pastors = res;
+          this.form_controls.pastor.enable(); //INHABILITAMOS EL SELECTOR DEL PASTOR
+          if (user) {
+            console.log(
+              'selector',
+              res.find((pt) => pt.user_code == user.pastor_code)
+            );
+            this.form_controls.pastor.setValue(
+              res.find((pt) => pt.user_code == user.pastor_code)
+            );
+            this.form_controls.pastor.disable();
+          }
+        },
+        (err) => {
+          throw err;
+        }
+      );
   }
 
   // CONSULTAMOS LOS LIDERES QUE PERTENECEN A LA RED DEL PASTOR
   getLeaders(pastor, user?) {
     this.leaders = []; //REINICIAMOS LOS LIDERES QUE CAMBIAN DE UN PASTOR A OTRO
-    this.userService.getLeadersOrPastors({ userCode: pastor.user_code, church: user ? user.church_id : this.currentUser.church_id}).subscribe(res => {
-      this.leaders = res;
-      this.add_user.get('leader').enable();//HABILITAMOS EL SELECTOR DEL LIDER
-      if (user) {
-        this.form_controls.leader.setValue(res.find(ld => ld.user_code == user.leader_code));
-      }
-    }, err => {
-      throw err;
-    });
+    this.userService
+      .getLeadersOrPastors({
+        userCode: pastor.user_code,
+        church: user ? user.church_id : this.currentUser.church_id,
+      })
+      .subscribe(
+        (res) => {
+          this.leaders = res;
+          this.add_user.get('leader').enable(); //HABILITAMOS EL SELECTOR DEL LIDER
+          if (user) {
+            this.form_controls.leader.setValue(
+              res.find((ld) => ld.user_code == user.leader_code)
+            );
+          }
+        },
+        (err) => {
+          throw err;
+        }
+      );
   }
 
   getChurchs(id) {
-    this.userService.getChurchById({ id }).subscribe(res => {
-      let city = [];
-      city.push(res)
-      this.add_user.get('church').setValue(city.find(ch => ch.id == id));
-      this.churchs = city;
-    }, err => { throw err });
+    this.userService.getChurchById({ id }).subscribe(
+      (res) => {
+        let city = [];
+        city.push(res);
+        this.add_user.get('church').setValue(city.find((ch) => ch.id == id));
+        this.churchs = city;
+      },
+      (err) => {
+        throw err;
+      }
+    );
   }
 
   searchUser(autocomplete?) {
     const filters = {};
-    if (this.form_value.document) { filters['identification'] = this.form_value.document.trim(); }
+    if (this.form_value.document) {
+      filters['identification'] = this.form_value.document.trim();
+    }
 
-    if (this.add_user.value.confirm_email && this.form_value.email == this.form_value.confirm_email) {
+    if (
+      this.add_user.value.confirm_email &&
+      this.form_value.email == this.form_value.confirm_email
+    ) {
       filters['email'] = this.form_value.email.trim().toLowerCase();
     }
 
-    this.userService.getUserInfo(filters).subscribe(res => {
-      console.log('user', res)
-      this.find_user = true;
-      if (autocomplete) {
-        this.setUser(res);
-      } else {
-        this.form_controls.id.setValue(res['id']);
-        this.createUser();
+    this.userService.getUserInfo(filters).subscribe(
+      (res) => {
+        console.log('user', res);
+        this.find_user = true;
+        if (autocomplete) {
+          this.setUser(res);
+        } else {
+          this.form_controls.id.setValue(res['id']);
+          this.createUser();
+        }
+      },
+      (err) => {
+        if (autocomplete) {
+          Swal.fire('No se encontro el usuario', '', 'info').then((res) => {
+            this.form_controls.network.enable();
+            this.form_controls.network.reset();
+            this.form_controls.pastor.reset();
+            this.form_controls.leader.reset();
+          });
+          throw err;
+        }
       }
-    }, err => {
-      if (autocomplete) {
-        Swal.fire('No se encontro el usuario', '', 'info').then(res => {
-          this.form_controls.network.enable();
-          this.form_controls.network.reset();
-          this.form_controls.pastor.reset();
-          this.form_controls.leader.reset();
-        });
-        throw err;
-      }
-    })
+    );
   }
-
-
 
   //SOLICITUD DE CREACION DE USUARIO
   addUserSend() {
     //VALIDAMOS LOS CAMPOS DEL USUARIO
     if (this.add_user.invalid) {
       Swal.fire('Datos incompletos', '', 'info');
-      return
+      return;
     }
     //CONFIRMAMOS EL EMAIL
     if (this.add_user.value.email != this.add_user.value.confirm_email) {
-      Swal.fire('Los correos no coinciden', '', 'info')
-      return
+      Swal.fire('Los correos no coinciden', '', 'info');
+      return;
     }
     //MOSTRAMOS EL LOADER\
     if (this.find_user) {
@@ -160,60 +208,82 @@ export class AddUserMassiveComponent implements OnInit {
         cancelButtonText: 'Cancelar',
         reverseButtons: true,
         showCancelButton: true,
-        showCloseButton: true
-      }).then(res => {
+        showCloseButton: true,
+      }).then((res) => {
         if (res.isConfirmed) {
           this.createUser();
         }
-      })
+      });
     } else {
       //VALIDAMOS QUE EL USUARIO EXISTA
       const filters = {};
-      if (this.form_value.document) { filters['identification'] = this.form_value.document.trim(); }
+      if (this.form_value.document) {
+        filters['identification'] = this.form_value.document.trim();
+      }
 
-      if (this.add_user.value.confirm_email && this.form_value.email == this.form_value.confirm_email) {
+      if (
+        this.add_user.value.confirm_email &&
+        this.form_value.email == this.form_value.confirm_email
+      ) {
         filters['email'] = this.form_value.email.trim().toLowerCase();
       }
 
-      this.userService.getUserInfo(filters).subscribe(res => {
-        console.log('user', res)
-        this.add_user.get('id').setValue(res['id'])
-        this.find_user = true;
-      }, err => {
-        this.createUser();
-
-      })
-
+      this.userService.getUserInfo(filters).subscribe(
+        (res) => {
+          console.log('user', res);
+          this.add_user.get('id').setValue(res['id']);
+          this.find_user = true;
+        },
+        (err) => {
+          this.createUser();
+        }
+      );
     }
-
   }
-
-
 
   createUser() {
     this.isLoading = true;
     //EJECUTAMOS EL ENDPOINT CON LOS DATOS DEL USUARIO
-    this.g12EventService.addUser(this.add_user.getRawValue()).subscribe(res => {
-      Swal.fire('Usuario Agregado', '', 'success').then(res => {
+    this.g12EventService.addUser(this.add_user.getRawValue()).subscribe(
+      (res) => {
+        Swal.fire('Usuario Agregado', '', 'success').then((res) => {
+          this.isLoading = false;
+          this.cdr.detectChanges();
+          this.modal.close(res);
+        });
+      },
+      (err) => {
         this.isLoading = false;
         this.cdr.detectChanges();
-        this.modal.close(res);
-      });
-    }, err => {
-      this.isLoading = false;
-      this.cdr.detectChanges();
-      Swal.fire(err ? err : 'No se pudo ejecutar la acción', '', 'error')
-    });
+        Swal.fire(err ? err : 'No se pudo ejecutar la acción', '', 'error');
+      }
+    );
   }
-
-
 
   //****************************/
   //CONTROLES DEL FORMULARIO
   //****************************/
 
-
   setUser(user) {
+    //VALIDAMOS EL TIPO DE USUARIO
+    switch (user?.type_church?.toString().toUpperCase()) {
+      //CASO MCI RENDERIZAMOS LOS USUARIOS
+      case 'MCI':
+        this.getPastors(user.pastor_code, user);
+        this.getLeaders({ user_code: user.leader_code }, user);
+        this.getChurchs(user.church_id);
+        this.form_controls.network.disable();
+
+        break;
+
+      default:
+        //SI NO ES HABILITAMOS LOS CAMPOS DE SELECCION
+        this.form_controls.network.enable();
+        this.form_controls.pastor.enable();
+        this.form_controls.leader.enable();
+        break;
+    }
+    this.form_controls.document.disable();
     this.form_controls.country.setValue(user.country);
     this.form_controls.id.setValue(user.id);
     this.form_controls.name.setValue(user.name);
@@ -224,15 +294,8 @@ export class AddUserMassiveComponent implements OnInit {
     this.form_controls.phone.setValue(user.phone);
     this.form_controls.network.setValue(user.network);
     this.form_controls.document.setValue(user.identification);
-    this.form_controls.document.disable();
     this.form_controls.document_type.setValue(user.document_type);
-    this.getPastors(user.pastor_code, user);
-    this.getLeaders({ user_code: user.leader_code }, user);
-    this.form_controls.network.disable();
-    this.getChurchs(user.church_id);
-
   }
-
 
   validateNumber(e) {
     if (e.key.match(/[0-9]/i) === null) {
@@ -249,5 +312,4 @@ export class AddUserMassiveComponent implements OnInit {
   get form_value() {
     return this.add_user.value;
   }
-
 }
