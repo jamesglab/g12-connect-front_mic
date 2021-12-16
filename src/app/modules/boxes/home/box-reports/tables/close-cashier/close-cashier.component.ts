c4ea3445bbcda699a13c53705da8791667c6a13e
Miguel torres <lt@wap.us>
2021-12-16 12:50:54 -0500

    download cashier box

--- src/app/modules/boxes/home/box-reports/tables/close-cashier/close-cashier.component.ts
+++ src/app/modules/boxes/home/box-reports/tables/close-cashier/close-cashier.component.ts
@@ -1,37 +1,8 @@
-import {
-  ChangeDetectorRef,
-  Component,
-  Input,
-  OnInit,
-  ViewChild,
-} from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
 import { ExportService } from 'src/app/modules/_services/export.service';
 import Swal from 'sweetalert2';
 import { BoxService } from '../../../_services/Boxes.service';
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-];
 
 @Component({
   selector: 'app-close-cashier',
@@ -137,9 +108,10 @@ export class CloseCashierComponent implements OnInit {
         'Total COP': this.calculate_position('total_cop'),
         'Total USD': this.calculate_position('total_usd'),
       });
+      console.log('BOXXXNAME',this.box)
       this.exportService.exportAsExcelFile(
         export_data,
-        `CIERRE_CAJERO_${this.box.name.toString().trim().toUpperCase()}`
+        `CIERRE_CAJERO_${this.box.name.toString().replace(/\s+/g, '').trim().toUpperCase()}`
       );
     } catch (error) {
       Swal.fire(
