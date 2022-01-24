a54ecd9dc27da67ef10359ea006147b23401b6e4
MiguelTRL <luisfb05@gmail.com>
2022-01-24 16:17:43 -0500

    delete validation of change report event

--- src/app/modules/g12events/users-not-pastor/components/change-event-user/change-event-user.component.ts
+++ src/app/modules/g12events/users-not-pastor/components/change-event-user/change-event-user.component.ts
@@ -1,21 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import Swal from 'sweetalert2';
-import { G12eventsService } from '../../../_services/g12events.service';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
+import Swal from "sweetalert2";
+import { G12eventsService } from "../../../_services/g12events.service";
 
 @Component({
-  selector: 'app-change-event-user',
-  templateUrl: './change-event-user.component.html',
-  styleUrls: ['./change-event-user.component.scss']
+  selector: "app-change-event-user",
+  templateUrl: "./change-event-user.component.html",
+  styleUrls: ["./change-event-user.component.scss"],
 })
 export class changeEventUserComponent implements OnInit {
   public report;
   public events;
   public editEventReport: FormGroup;
   public loader: boolean;
-  constructor(private fb: FormBuilder, private _g12Events: G12eventsService,
-    public modal: NgbActiveModal) { }
+  constructor(
+    private fb: FormBuilder,
+    private _g12Events: G12eventsService,
+    public modal: NgbActiveModal
+  ) {}
 
   ngOnInit(): void {
     this.buildForm();
@@ -26,28 +29,33 @@ export class changeEventUserComponent implements OnInit {
       donation: [null, Validators.required],
       financial_cut: [null, Validators.required],
       description: [null, Validators.required],
-      transaction: [this.report.transaction.id]
+      transaction: [this.report.transaction.id],
     });
   }
   onSubmit() {
     if (this.editEventReport.valid) {
       this.loader = true;
-      if (parseInt(this.report?.transaction?.amount)
-        == parseInt(this.editEventReport.getRawValue().financial_cut.prices[this.report?.transaction?.currency?.toLowerCase()])) {
-        this._g12Events.changeReport(this.editEventReport.getRawValue()).subscribe(res => {
-          this.loader = false;
-          this.modal.close(true);
-          Swal.fire('Reporte Actualizado', '', 'success');
-        }, err => {
-          this.loader = false;
-          Swal.fire('No se pudo actualizar', err, 'error');
-        });
-      } else {
-        this.loader = false;
-        Swal.fire('No se pudo actualizar', 'El precio de la transaccion y el precio del corte no coinciden', 'info');
-      }
+      // if (parseInt(this.report?.transaction?.amount)
+      //  == parseInt(this.editEventReport.getRawValue().financial_cut.prices[this.report?.transaction?.currency?.toLowerCase()])) {
+      this._g12Events
+        .changeReport(this.editEventReport.getRawValue())
+        .subscribe(
+          (res) => {
+            this.loader = false;
+            this.modal.close(true);
+            Swal.fire("Reporte Actualizado", "", "success");
+          },
+          (err) => {
+            this.loader = false;
+            Swal.fire("No se pudo actualizar", err, "error");
+          }
+        );
+      //    } else {
+      //    this.loader = false;
+      //  Swal.fire('No se pudo actualizar', 'El precio de la transaccion y el precio del corte no coinciden', 'info');
+      //}
     } else {
-      Swal.fire('Valida los campos', '', 'warning');
+      Swal.fire("Valida los campos", "", "warning");
     }
   }
 }
